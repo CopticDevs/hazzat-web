cbd24da0a918ebcc63dd907d8e5c6304cf178324
Fady Wahba <fadyw@microsoft.com>
2022-03-28 20:18:57 -0700

    Fix unit tests

--- src/Home.test.tsx
+++ src/Home.test.tsx
@@ -1,9 +1,12 @@
-import React from 'react';
+import { BrowserRouter } from "react-router-dom";
 import { render, screen } from '@testing-library/react';
 import Home from './Home';
 
 test('renders seasons link', () => {
-  render(<Home />);
+  render(<BrowserRouter >
+    <Home />
+  </BrowserRouter >
+  );
   const linkElement = screen.getByText(/Seasons/i);
   expect(linkElement).toBeInTheDocument();
 });
