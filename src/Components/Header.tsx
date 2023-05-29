a6cd17f5159ea7b75642779e480353d8c5fc7bb2
Fady Wahba <fadyham@hotmail.com>
2023-05-28 19:33:08 -0700

    removed unused var

--- src/Components/Header.tsx
+++ src/Components/Header.tsx
@@ -1,4 +1,4 @@
-import { faBars, faChevronDown } from "@fortawesome/free-solid-svg-icons";
+import { faBars } from "@fortawesome/free-solid-svg-icons";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { useContext, useState } from "react";
 import logo from "../images/logo.png";
