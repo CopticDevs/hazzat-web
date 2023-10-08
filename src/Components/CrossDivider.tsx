5686d152924964b5b409751cc68519677656dc78
fadyham <fadyham@hotmail.com>
2023-10-08 01:34:35 -0700

    Fixed text content (#108)
    Co-authored-by: Fady Wahba <fadyw@microsoft.com>

--- src/Components/CrossDivider.tsx
+++ src/Components/CrossDivider.tsx
@@ -1,10 +1,22 @@
-import { ReactComponent as CrossLine } from "../images/crossline.svg";
+import { ReactComponent as CrossLineImg } from "../images/crossline.svg";
+import { ReactComponent as CrossesDividerImg } from "../images/crossesDivider.svg";
 
-function CrossDivider() {
+interface IProps {
+    isMini?: boolean;
+}
+function CrossDivider(props: IProps) {
+
+    if (props.isMini) {
+        return (
+            <div style={{ textAlign: "center" }}>
+                <CrossesDividerImg style={{ width: "50px", marginBottom: "20px" }} />
+            </div>
+        );
+    }
 
     return (
         <div style={{ textAlign: "center" }}>
-            <CrossLine style={{ width: "70%", maxWidth: "508px", marginTop: "10px", marginBottom: "10px" }} />
+            <CrossLineImg style={{ width: "70%", maxWidth: "508px", marginTop: "10px", marginBottom: "10px" }} />
         </div>
     );
 }
